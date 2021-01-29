d235769de92ce8b87d7ac6b37276b9660e179304
Mor Dahan <mor.dahan@campus.technion.ac.il>
2021-01-29 14:26:51 +0200

    fix permissions

--- firewall.sh
+++ firewall.sh
@@ -2,7 +2,7 @@
 
 # exit if the number of arguments different from 1
 if [ "$#" -ne 1 ]; then
-exit
+	exit
 fi
 
 #insert stdin into txt file
@@ -15,14 +15,13 @@ while read line;
 do
 	IFS=',' read -r -a subrules <<< "$line"
 	the_rules=$(cat stdin.txt | \
-       ./firewall.exe "${subrules[0]}" 2>/dev/null | \
-       ./firewall.exe "${subrules[1]}" 2>/dev/null| \
-       ./firewall.exe "${subrules[2]}" 2>/dev/null| \
-       ./firewall.exe "${subrules[3]}" 2>/dev/null)
+	./firewall.exe "${subrules[0]}" 2>/dev/null | \
+	./firewall.exe "${subrules[1]}" 2>/dev/null| \
+	./firewall.exe "${subrules[2]}" 2>/dev/null| \
+	./firewall.exe "${subrules[3]}" 2>/dev/null)
 	echo "$the_rules" >> bin.txt
-
 done < new_rules.txt
 
-cat bin.txt | sort | uniq | tr -d " \t" 
+cat bin.txt | sort | uniq | tr -d " \t"
 
 rm -f bin.txt stdin.txt new_rules.txt
